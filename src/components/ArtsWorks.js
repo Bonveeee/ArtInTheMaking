65474f86834b5285c976cbc1ef244231b4f7f702
bonveeee <bonochieng@gmail.com>
2022-07-29 03:35:37 +0300

    fetch artworks data

--- src/components/ArtsWorks.js
+++ src/components/ArtsWorks.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
+
+const ARTWRKS_URL = "https://api.artic.edu/api/v1/artworks?fields=id,title,date_display,date_display,main_reference_number,dimensions,artist_id,artist_title,artwork_type_title,image_id,medium_display,dimensions,place_of_origin,thumbnail,term_titles"
+
 
 function ArtsWorks() {
+
+  const [artsWorks, setArtWorks] = useState([])
+
+  const artworksFetcher = () => {
+    fetch(ARTWRKS_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.data);
+        setArtWorks(data);
+      });
+  };
+
+  useEffect(artworksFetcher, []);
+
+  const image_id = artsWorks.image_id
+  console.log(image_id)
+//const imageUrl = "https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg"  //Const imageUrl= "/"+config.ii_url +"/"+ image_id + "anything here"
+//https://www.artic.edu/iiif/2/24347d10-6df9-589e-5818-46ddcf18966f/full/843,/0/default.jpg
+const imageUrl= `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
+
+const eachArtWorks = artsWorks.data.map((arts)  =>(
+    //   console.log(arts)
+    <div className='card-images' key={artsWorks.id}>
+      <div className='cardsimages'>
+           <img className='img' src={imageUrl} alt ="artImage" />
+        <h2>{artsWorks.date_display}</h2>
+        <h2>{artsWorks.artist_title}</h2>
+        <h2>{artsWorks.medium_display}</h2>
+        <h2>{artsWorks.dimensions}</h2>
+      </div>
+    </div>
+ 
+ 
+  ))
+
   return (
-    <div>ArtsWorks</div>
+    <div>{eachArtWorks}</div>
   )
 }
 
