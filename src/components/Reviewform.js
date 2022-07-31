ddc8b5b5ae26c3616131db553a9ab1eb21530194
bonveeee <bonochieng@gmail.com>
2022-07-31 19:29:18 +0300

    updates on reviewform and search components

--- src/components/Reviewform.js
+++ src/components/Reviewform.js
@@ -1,24 +1,27 @@
-import React from 'react'
+
+import React ,{useState} from 'react'
 
 function Reviewform() {
+  const [text, setText] = useState(" ");
 
     function handleSubmit(event){
         event.preventDefault()
     }
 
     function handleChange(event){
-        // console.log(event.target.name)
-    }
+                     setText(event.target.value)}
+           // console.log(event.target.name)
+    
 
   return (
     <div class="form-center"> 
          <form className="review-form" onSubmit={handleSubmit} >
              <h1>Reviews</h1>
-              <textarea name="content" onChange={handleChange} placeholder="Write your review here..." rows={10} />
-             <input type="submit" value="Send your review" />
+              <textarea name="content"  onChange={handleChange} placeholder="Write your review here..." rows={10} />
+             <input type="submit"  onSubmit={handleSubmit} value="Send your review" />
              <p>Sign up for the latest updates in contemporary Art & Design</p>
              <input type="email" name= "email" onChange={handleChange} placeholder="Email Us" />
-            <input type="submit" value="Sign Up" />
+            <input type="submit" onSubmit={handleSubmit} value="Sign Up" />
          </form>
   </div>
    
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,55 +1,55 @@
 import React, { useState, useEffect } from "react";
 
 //this search component will make users search for keywords on the site
-const SEARCH = "https://api.artic.edu/api/v1/artworks/search?"
+const SEARCH = "https://api.artic.edu/api/v1/artworks/search?";
 
 function Search() {
+  const [searchItem, setSearchItem] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [results, setResults] = useState(0);
 
-  const [searchItem, setSearchItem] = useState("")
-    const [counter, setCounter] = useState(0)
-    const [results, setResults] = useState(0)
+  const handleSearch = (event) => {
+    setSearchItem(event.target.value);
+  };
 
-    const handleSearch = (event) => {
-        setSearchItem(event.target.value)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCounter(counter + 1);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setCounter(counter + 1)
-    }
+  const searchInfo = () => {
+    const actualSearch = SEARCH + searchItem;
 
-    const searchInfo = () => {
-
-      const actualSearch = SEARCH + searchItem
-
-      fetch(actualSearch)
+    fetch(actualSearch)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
-          setSearchItem("")
-          setResults(data.data== null ? 0 : data.data.length )
-              })
-  }
+        console.log(data);
+        setSearchItem("");
+        setResults(data.data == null ? 0 : data.data.length);
+      });
+  };
 
-  useEffect(
-      searchInfo, [counter]
-  )
-
-
+  useEffect(searchInfo);
 
   return (
     <div className="search-container">
-            <form onSubmit={handleSubmit}>
-                <div>
-                   <input type="text" placeholder="What are you looking for?" className="form-control" id="search" name="search" value={searchItem} onChange={handleSearch}/>
-                </div>
-            </form>
-            <h3>Available results({results})</h3>
-            <div className="row">
-                {/* TODO: POPULATE RESULTS */}
-            </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="What are you looking for?"
+            className="form-control"
+            id="search"
+            name="search"
+            value={searchItem}
+            onChange={handleSearch}
+          />
         </div>
-  )
+      </form>
+      <h3>Available results({results})</h3>
+      <div className="row"></div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
